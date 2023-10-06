8a91d8c24195a94d2aea734ab71fb90b831e6ca0
durgarao <durgarao81813@gmail.com>
2023-10-06 11:04:29 +0530

    ui updated

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Cookies from 'js-cookie'
 import { Navigate } from 'react-router-dom'
+import axios from 'axios'
 
-export default function Cart() 
-
-{
+export default function Cart() {
     const jwtToken = Cookies.get("jwtToken")
+    const [cartdata,setcartdata]=useState()
+    useEffect(()=>{
+        const function1=async ()=>{
+            try {
+                const response = await axios.get("http://localhost:5000/cart")
+                
+                console.log(response)
+        
+              } catch (error) {
+                console.error('Error fetching data:', error);
+              }
+
+
+
+        }
+
+        function1()
+    })
+
+
     if(jwtToken===undefined){
         return <Navigate to="/login" />
 
@@ -14,7 +33,8 @@ export default function Cart()
         return (
             <div>
                 <center><h1>CART</h1></center>
-                <div>
+                <div className='card shadow m-5 border-dark card-box'>
+
         
                 </div>
               
